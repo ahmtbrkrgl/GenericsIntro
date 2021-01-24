cc9cebbddce6748ce780d7d4a20f73402cccd5f8
Ahmet Burak Karagöl <ahmtbrkrgl@hotmail.com>
2021-01-24 14:52:52 +0300

    Add project files.

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        T[] tempItems;
+
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+        public void Add(T item)
+        {
+            tempItems = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+            }
+            items[items.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return items.Length; }
+            
+        }
+
+        public T[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> sehirler = new MyList<string>();
+            sehirler.Add("Sivas");
+
+            Console.WriteLine(sehirler.Count);
+
+            sehirler.Add("Tokat");
+
+            Console.WriteLine(sehirler.Count);
+
+            sehirler.Add("Trabzon");
+
+            Console.WriteLine(sehirler.Count);
+
+            foreach (var sehir in sehirler.Items)
+            {
+                Console.WriteLine(sehir);
+            }
+
+        }
+    }
+}
